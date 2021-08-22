d6637156b9d86302b9c3515214978129d43a1768
Dzenis_Pepic <dzenisinedz@gmail.com>
2021-08-22 18:25:04 +0200

    projekat

---
+++ puppycompanyblog/__init__.py
@@ -0,0 +1,50 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'mysecret'
+
+
+# Ovo je jedan nacin da se povezemo sa bazom
+#basedir = os.path.abspath(os.path.dirname(__file__))
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+# Ovo je drugi nacin da se povezemo sa bazom, da bismo videli sta se desava vizuelno!
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://sql11431185:ZcE4ExpibM@sql11.freemysqlhosting.net/sql11431185'
+
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+
+
+
+login_manager = LoginManager()
+
+
+login_manager.init_app(app)
+
+
+login_manager.login_view = "users.login"
+
+
+
+from puppycompanyblog.core.views import core
+from puppycompanyblog.users.views import users
+from puppycompanyblog.blog_posts.views import blog_posts
+from puppycompanyblog.error_pages.handlers import error_pages
+
+
+app.register_blueprint(users)
+app.register_blueprint(blog_posts)
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
